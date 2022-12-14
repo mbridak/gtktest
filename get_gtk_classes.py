e097c4047157ffbd518f74a00a25d5758ec21b5b
mbridak <michael.bridak@gmail.com>
2022-12-14 12:24:13 -0800

    Hide properties in common with Gtk.Widget
    If the class was derived from Gtk.Widget, items in common were removed.
    This shortened the file by about 40k lines.

--- get_gtk_classes.py
+++ get_gtk_classes.py
@@ -4,28 +4,38 @@
 # pylint: disable=no-member
 # pylint: disable=eval-used
 
-import sys
 import gi
 
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk
 
+widget_dir = dir(Gtk.Widget)
+
 with open("proplist.md", "w", encoding="utf-8") as f:
     print("<!-- markdownlint-disable no-inline-html -->", file=f)
+    print("<!-- markdownlint-disable no-multiple-blanks -->", file=f)
+    print("<!-- markdownlint-disable no-trailing-spaces -->", file=f)
     print("# Gtk Class List", file=f)
+    print(
+        "\r\nIf class derived from Gtk.Widget, properties from Gtk.Widget are omitted.\r\n\r\n---",
+        file=f,
+    )
     gtk_items = dir(Gtk)
     for item in gtk_items:
         gtk_item = eval(f"Gtk.{item}")
         if isinstance(eval(f"Gtk.{item}"), gi.types.GObjectMeta):
             thedoc = repr(gtk_item.__doc__)
             thedoc = thedoc.replace("'", "")
-            thedoc = thedoc.replace("\\n", "\r\n\t")
+            thedoc = thedoc.replace("\\n", "\r\n    ")
 
             print(
-                f"\r\n<details><summary>Gtk.{item}</summary>\r\n\r\n{thedoc}\r\n\r\n---\r\n",
+                f"\r\n<details><summary>Gtk.{item}</summary>\r\n\r\n"
+                f"{thedoc}\r\n\r\n---\r\n",
                 file=f,
             )
             proplist = dir(gtk_item)
+            derived_from_widget = "Gtk.Widget" in thedoc
             for propitem in proplist:
-                print(f"- {propitem}", file=f)
+                if propitem not in widget_dir or not derived_from_widget:
+                    print(f"- {propitem}", file=f)
             print("\r\n</details>", file=f)
